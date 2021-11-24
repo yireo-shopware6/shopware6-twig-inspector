2e92a7670d6b4c9062ef04c19c08a69017f4c787
jisse Reitsma <jisse@yireo.com>
2021-11-24 08:59:34 +0100

    Become a programmer, they said. It'll be fun, they said.

--- src/YireoTwigInspector.php
+++ src/YireoTwigInspector.php
@@ -2,9 +2,16 @@
 
 namespace Yireo\TwigInspector;
 
+use Oro\TwigInspector\Bundle\OroTwigInspectorBundle;
+use Shopware\Core\Framework\Parameter\AdditionalBundleParameters;
 use Shopware\Core\Framework\Plugin;
 
 class YireoTwigInspector extends Plugin
 {
-
+    public function getAdditionalBundles(AdditionalBundleParameters $parameters): array
+    {
+        return [
+            new OroTwigInspectorBundle
+        ];
+    }
 }
